f71c92369e542eca69c20abcf10e91e3e798de56
heichong <hnzb521@126.com>
2016-12-09 08:15:27 +0800

    读数据库记录写文件

---
+++ src/main/java/com/me/springbatch/db2file/DB2FileMain.java
@@ -0,0 +1,12 @@
+package com.me.springbatch.db2file;
+
+import com.me.springbatch.App;
+
+public class DB2FileMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		App.run("readDB2FileJob");
+	}
+
+}
